320020009b400cc9792f7d86d60c8ef7523eb954
zsitole <oleksii.sitnianskyi.ext@ericsson.com>
2023-10-25 16:48:50 +0300

    Initial commit

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+    </head>
+      <body>
+        <h1>GCP Cloud Build Prod test image</h1>
+ <?php
+        $ip_server = $_SERVER['SERVER_ADDR'];
+  
+        echo "<br>Server IP Address is: $ip_server" 
+  ?>
+              <p>
+  
+               <b>
+                   <font color="red">Application Version 1.3
+                 </b>
+                   </font>
+              </p>
+       </body>
+   </html>
